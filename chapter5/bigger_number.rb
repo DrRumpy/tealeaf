3b9de9ff756352331b29960cb1cfd0ff8f105616
DrRumpy <rumpy@mrbitter.org>
2014-03-22 23:05:06 -0700

    adding chapter exercises

---
+++ chapter5/bigger_number.rb
@@ -0,0 +1,4 @@
+puts 'Type your favorite number, please'
+number = gets.chomp
+bigger = number.to_i + 1
+puts 'I think you\'ll find ' + bigger.to_s + ' is an even bigger, better favorite number!.'
---
+++ chapter5/get_first_last.rb
@@ -0,0 +1,5 @@
+puts 'What is your first name?'
+first = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'It\'s very nice to meet you ' + first + ' ' + last + '.'
