979c54216a73838e8c7c0e1c49d22d1d9c525b8e
Josh Eror <josh@perforce.com>
2014-03-27 18:07:05 -0700

    some chapter 9 exercises

---
+++ chapter9/local_var.rb
@@ -0,0 +1,6 @@
+def double_this num
+	num_times_2 = num*2
+	puts num.to_s + ' is doubled ' + num_times_2.to_s
+end
+
+double_this 44
---
+++ chapter9/moos.rb
@@ -0,0 +1,8 @@
+def say_moo number_of_moos
+	puts 'mooooooooo...' * number_of_moos
+end
+
+say_moo 3
+puts 'oink-oink'
+puts
+say_moo
