dd672b5a5df3682059e73260a7390f4c54de5a8d
DrRumpy <rumpy@mrbitter.org>
2014-04-01 00:29:46 -0700

    still working on sorting sans recursion

--- chapter10/sort_no_recursion.rb
+++ chapter10/sort_no_recursion.rb
@@ -1,32 +1,41 @@
 def sort_array to_sort
 	sorted = []
 	holder = []
+	length = to_sort.length
 
-while sorted.length < to_sort.length
+	while true
+		check = nil
 		holder.push(to_sort.pop)
+
 		to_sort.each do |i|
-			if holder < i
+			if holder[0] < i
 				check = true
-			elsif holder > i
+			elsif holder[0] > i
 				check = false
+				break
 			end
+		end
 
 		if check == true
 			sorted.push(holder.pop)
-		else
-			to_sort.push(holder.pop)
-			#need to use array insert method
+		elsif check == false
+			to_sort.insert(0, holder.pop)
+		end
+
+		if sorted.length == 4
+			puts sorted
+			puts length
+			# I haven't dealt with to_sort being empty and 
+			# holder being compared against the now empty
+			# array
+			break
+		end
 
 
-		
 	end
+
+	
 end
 
-
-
-puts holder
-
-end
-
-foo = ['zebra', 'tango', 'delta', 'dog', 'mike']
+foo = ['zebra', 'delta', 'dog', 'tango', 'mike']
 sort_array foo
