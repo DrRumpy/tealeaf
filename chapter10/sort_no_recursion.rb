6eb20333a1a8edc3815661d9b1b1af339f28e1f6
Josh Eror <josh@perforce.com>
2014-04-01 13:50:53 -0700

    chapter 10 sort no recursion - done!

--- chapter10/sort_no_recursion.rb
+++ chapter10/sort_no_recursion.rb
@@ -8,7 +8,7 @@ def sort_array to_sort
 		holder.push(to_sort.pop)
 
 		to_sort.each do |i|
-			if holder[0] < i
+			if holder[0] <= i
 				check = true
 			elsif holder[0] > i
 				check = false
@@ -16,26 +16,22 @@ def sort_array to_sort
 			end
 		end
 
-		if check == true
+		if to_sort.length == 0
+			sorted.push(holder.pop)
+		elsif check == true
 			sorted.push(holder.pop)
 		elsif check == false
 			to_sort.insert(0, holder.pop)
 		end
 
-		if sorted.length == 4
-			puts sorted
-			puts length
-			# I haven't dealt with to_sort being empty and 
-			# holder being compared against the now empty
-			# array
+		if sorted.length == length
 			break
 		end
-
-
 	end
 
+return sorted
 	
 end
 
-foo = ['zebra', 'delta', 'dog', 'tango', 'mike']
-sort_array foo
+foo = ['zebra', 'delta', 'dog', 'dog', 'tango', 'mike']
+puts sort_array foo
