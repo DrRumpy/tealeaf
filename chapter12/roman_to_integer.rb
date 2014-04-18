bd0402bf54e9ae4bf56575dc1c83e91d09b50656
Josh Eror <josh@perforce.com>
2014-04-17 18:19:40 -0700

    fiddling with exercise

--- chapter12/roman_to_integer.rb
+++ chapter12/roman_to_integer.rb
@@ -1,20 +1,48 @@
-roman_numerals = {'m' => 1000,
-                  'd' =>  500,
-                  'c' =>  100,
-                  'l' =>   50,
-                  'x' =>   10,
-                  'v' =>    5,
-                  'i' =>    1}
+roman_numerals = {'m'  => 1000,
+                  'cm' =>  900,
+                  'd'  =>  500,
+                  'cd' =>  400,
+                  'c'  =>  100,
+                  'xc' =>   90,
+                  'l'  =>   50,
+                  'xl' =>   40,
+                  'x'  =>   10,
+                  'ix' =>    9,
+                  'v'  =>    5,
+                  'iv' =>    4,
+                  'i'  =>    1}
 
-puts "Please enter a Roman numeral"
+def numeral_value num_arr, roman_numerals
+  total = 0
+
+  num_arr.each do |num|
+    total += roman_numerals[num] 
+  end
+
+  total
+end
+
+def remove_fours_nines num_arr, roman_numerals
+# identify and separate 4's and 9's from roman 
+# numeral entered by user.
+num_str = num_arr.to_s
+  
+
+
 while true
-	num = gets.chomp
-  counter = 0
-  num.length.times do |char|
+  count = 0
+  puts "Please enter a Roman numeral"
+  num_arr = gets.chomp.scan(/\w/)
+  num_arr.each do |char|
     if roman_numerals.include?(char)
-      next
+      count += 1
     else
-      puts "Please enter a valid Roman numeral"
+      break
     end
+  end
+  
+  if count == num_arr.length
+    puts numeral_value num_arr, roman_numerals
+    break
   end
 end
