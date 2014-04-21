31be908cdb195a1559484611046a6474f5e66d71
DrRumpy <rumpy@mrbitter.org>
2014-04-21 01:15:06 -0700

    Finished chapter 12 exercises

--- chapter12/birthdays.rb
+++ chapter12/birthdays.rb
@@ -1,3 +1,6 @@
+
+
+seconds_in_year = 31536000
 birth_dates = {}
 
 filename = 'birthdays.txt'
@@ -9,5 +12,34 @@ read_string.each_line do |line|
   birth_dates[before] = after.lstrip
 end
 
-puts birth_dates
+puts "Please enter a name?"
+name = gets.chomp
 
+months = {'Jan' => 1,
+          'Feb' => 2,
+          'Mar' => 3,
+          'Apr' => 4,
+          'May' => 5,
+          'Jun' => 6,
+          'Jul' => 7,
+          'Aug' => 8,
+          'Sep' => 9,
+          'Oct' => 10,
+          'Nov' => 11,
+          'Dec' => 12}
+
+year_array = birth_dates[name].split
+month  = year_array[0]
+day    = year_array[1].chop
+year   = year_array[2]
+
+years_old = ((Time.new - Time.local(year, month, day)) / seconds_in_year).to_i
+current_year = Time.now.year
+
+#have we missed this years bday?
+if (Time.local(current_year, month, day) - Time.now) < 0
+  current_year += 1
+end
+
+puts "#{name}'s next Birthday is #{month} #{day} #{current_year}."
+puts "#{name} will #{years_old}! Yay!"
