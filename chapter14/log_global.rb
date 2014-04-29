f35070f7026352ba7a31aff9d67eeae2560f9228
DrRumpy <rumpy@mrbitter.org>
2014-04-28 22:56:56 -0700

    last exercise in chpater 14. Book complete!

--- chapter14/log_global.rb
+++ chapter14/log_global.rb
@@ -1,18 +1,22 @@
+$nest_depth = 0
+
 def log(description, &block)
-  $nest_depth = 0
 
   puts "Begin #{description}".insert(0, ("\s" * $nest_depth))
   $nest_depth += 1
-  block.call
-  puts "Begin #{description}".insert(0, ("\s" * $nest_depth))
+
+  result = block.call
+  puts result.to_s.insert(0, ("\s" * $nest_depth))
+  
   $nest_depth -= 1
+  puts "End #{description}".insert(0, ("\s" * $nest_depth))
 
 end
 
 log '1st Level Block...' do 
   log '2nd Level Block...' do
-      puts "Begin 3rd Level Block..."
-      puts "You're deep in it now!"
-      puts "End 3rd Level Block..."
+    log '3rd Level Block...' do 
+      "You're deep in it now!"
+    end
   end
 end
